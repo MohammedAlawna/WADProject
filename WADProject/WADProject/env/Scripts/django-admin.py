edbad1555d3d366970e16afeea50b4af95961b88
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-06-17 17:56:07 +0100

    addedWADApp.
    - WAD app added to the project.

---
+++ WADProject/WADProject/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Git\WADProject\WADProject\WADProject\WADProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
